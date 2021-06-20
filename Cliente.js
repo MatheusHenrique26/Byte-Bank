ae3399e58d078ef9d49fbbb7cf84b3a7f4daf6cf
Matheus Henrique <19matheushenrique03@gmail.com>
2021-06-20 13:36:56 -0300

    Update Cliente.js

--- Cliente.js
+++ Cliente.js
@@ -1,14 +1,13 @@
 export class cliente{
-    nome;
-    _cpf;  
-    
     get cpf(){
         return this._cpf;
     }
 
-    constructor(nome , cpf){
+    constructor(nome , cpf, senha){
         this.nome = nome;
         this._cpf = cpf;
+        this._senha = senha
            
     }
+
 }
